d7c04637ced64202496bbc7d104fb6cfea052856
Jeonghan-Shin <alexsjh23@gmail.com>
2021-07-09 15:43:05 +0900

    Skill 추가 - Combination

---
+++ src/skills/Combination.java
@@ -0,0 +1,58 @@
+package skills;
+
+public class Combination {
+
+    public static void main(String[] args) {
+        Combination comb = new Combination();
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int len = arr.length;
+        boolean[] visit = new boolean[len];
+
+        // Combination01 - 백트래킹
+        comb.combination01(arr, visit, 0, 0, len, 3);
+
+        // Combination02 - 재귀
+        comb.combination02(arr, visit, 0, 0, len, 2);
+    }
+
+    // <백트래킹을 이용한 조합>
+    public void combination01(int[] arr, boolean[] visit, int idx, int cnt, int n, int r) {
+        if (cnt == r) {
+            print(arr, visit, n);
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            visit[i] = true;
+            combination01(arr, visit, i + 1, cnt + 1, n, r);
+            visit[i] = false;
+        }
+    }
+
+    // <재귀를 이용한 조합>
+    public void combination02(int[] arr, boolean[] visit, int idx, int cnt, int n, int r) {
+        if (cnt == r) {
+            print(arr, visit, n);
+            return;
+        }
+
+        if (idx == n)
+            return;
+
+        visit[idx] = true;
+        combination02(arr, visit, idx + 1, cnt + 1, n, r);
+        visit[idx] = false;
+        combination02(arr, visit, idx + 1, cnt, n, r);
+
+    }
+
+    private void print(int[] arr, boolean[] visit, int n) {
+        for (int i = 0; i < n; i++) {
+            if (visit[i])
+                System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
