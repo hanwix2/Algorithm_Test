0c3902d7e23bcd661b3a1de509c2ed3bf38b0fc1
Jeonghan-Shin <alexsjh23@gmail.com>
2021-07-09 16:21:05 +0900

    Skill - Combination 수정
    - visit 배열이 아닌 result 리스트를 이용해 조합 만들기 적용

--- src/skills/Combination.java
+++ src/skills/Combination.java
@@ -1,5 +1,8 @@
 package skills;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Combination {
 
     public static void main(String[] args) {
@@ -14,7 +17,8 @@ public class Combination {
         comb.combination01(arr, visit, 0, 0, len, 3);
 
         // Combination02 - 재귀
-        comb.combination02(arr, visit, 0, 0, len, 2);
+        List<Integer> result = new ArrayList<>();
+        comb.combination02(arr, result, 0, 0, len, 2);
     }
 
     // <백트래킹을 이용한 조합>
@@ -32,21 +36,28 @@ public class Combination {
     }
 
     // <재귀를 이용한 조합>
-    public void combination02(int[] arr, boolean[] visit, int idx, int cnt, int n, int r) {
+    public void combination02(int[] arr, List<Integer> result, int idx, int cnt, int n, int r) {
         if (cnt == r) {
-            print(arr, visit, n);
+            print(result);
             return;
         }
 
         if (idx == n)
             return;
 
-        visit[idx] = true;
-        combination02(arr, visit, idx + 1, cnt + 1, n, r);
-        visit[idx] = false;
-        combination02(arr, visit, idx + 1, cnt, n, r);
-
+        result.add(arr[idx]);
+        combination02(arr, result, idx + 1, cnt + 1, n, r);
+        result.remove(result.size() - 1);
+        combination02(arr, result, idx + 1, cnt, n, r);
     }
+
+    private void print(List<Integer> result) {
+        for (Integer num : result)
+            System.out.print(num + " ");
+
+        System.out.println();
+    }
+
 
     private void print(int[] arr, boolean[] visit, int n) {
         for (int i = 0; i < n; i++) {
